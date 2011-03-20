f0ddf14942d7bb0dc28104402c0c9eb4d476b4f5
Roman Shestakov <romanshestakov@yahoo.co.uk>
2011-03-20 12:56:03 +0000

    add in-out degree check

--- test/mdigraph_SUITE.erl
+++ test/mdigraph_SUITE.erl
@@ -53,25 +53,56 @@ end_per_testcase(_TestCase, Config) ->
 
 all() -> 
     [
-     add_vertex,
+     add,
+     source_sink,
+     in_out_degree,
      del_vertex
     ].
 
-add_vertex(Config) ->
+add(Config) ->
     MG = ?config(mg, Config),
     DG = ?config(dg, Config),
-
     %% compare vertices could be in unspecified order
     MG_V = lists:sort(mdigraph:vertices(MG)),
     DG_V = lists:sort(digraph:vertices(DG)),
     ct:log("-> vertices, ~p, ~p ", [MG_V, DG_V]),
     MG_V = DG_V,
-
     %% compare edges
     MG_E = lists:sort(mdigraph:edges(MG)),
     DG_E = lists:sort(digraph:edges(DG)),
     ct:log("-> edges, ~p, ~p ", [MG_E, DG_E]),
     MG_E = DG_E,
+    ok.
+
+
+source_sink(Config) ->
+    MG = ?config(mg, Config),
+    DG = ?config(dg, Config),
+    %% source
+    MG_Source = mdigraph:source_vertices(MG),
+    DG_Source = digraph:source_vertices(DG),
+    ct:log("-> source_vertices, ~p, ~p ", [MG_Source, DG_Source]),
+    MG_Source = DG_Source,
+    %% sink
+    MG_Sink = mdigraph:sink_vertices(MG),
+    DG_Sink = digraph:sink_vertices(DG),
+    ct:log("-> sink_vertices, ~p, ~p ", [MG_Sink, DG_Sink]),
+    ok.
+
+in_out_degree(Config) ->
+    MG = ?config(mg, Config),
+    DG = ?config(dg, Config),
+    Vertices = ?config(vertices, Config),
+    %% in degree
+    MG_In_degree = [ {V, mdigraph:in_degree(MG, V)} || V <- Vertices ],
+    DG_In_degree = [ {V, digraph:in_degree(DG, V)} || V <- Vertices ],
+    ct:log("-> In_degree, ~p, ~p ", [MG_In_degree, DG_In_degree]),
+    MG_In_degree = DG_In_degree,
+    %% out degree
+    MG_Out_degree = [ {V, mdigraph:out_degree(MG, V)} || V <- Vertices ],
+    DG_Out_degree = [ {V, digraph:out_degree(DG, V)} || V <- Vertices ],
+    ct:log("-> Out_degree, ~p, ~p ", [MG_Out_degree, DG_Out_degree]),
+    MG_Out_degree = DG_Out_degree,
     ok.
 
 
