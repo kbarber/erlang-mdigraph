b341479d40fc947f12bb2b4cf15936b0bd41ac8f
Roman Shestakov <romanshestakov@yahoo.co.uk>
2011-12-12 22:55:08 +0000

    move mdigraph_test to test folder. remove tests which rely on pattern matching on opaque type.

--- test/mdigraph_tests.erl
+++ test/mdigraph_tests.erl
@@ -11,16 +11,6 @@
 new_test_() ->
     {setup, fun() -> mnesia:start() end, fun(ok) -> mnesia:stop() end,  
      [
-      ?_assertMatch({mdigraph, _, _, _, true},  mdigraph:new()),
-      ?_assertMatch({mdigraph, _, _, _, true},  mdigraph:new([])),
-      ?_assertMatch({mdigraph, _, _, _, true},  mdigraph:new([cyclic])),
-      ?_assertMatch({mdigraph, _, _, _, false}, mdigraph:new([acyclic])),
-      ?_assertMatch({mdigraph, _, _, _, true},  mdigraph:new([protected])),
-      ?_assertMatch({mdigraph, _, _, _, true},  mdigraph:new([private])),
-      ?_assertMatch({mdigraph, _, _, _, true},  mdigraph:new([cyclic, protected])), 
-      ?_assertMatch({mdigraph, _, _, _, true},  mdigraph:new([cyclic, private])), 
-      ?_assertMatch({mdigraph, _, _, _, false}, mdigraph:new([acyclic, protected])), 
-      ?_assertMatch({mdigraph, _, _, _, false}, mdigraph:new([acyclic, private])),
       ?_assertError(badarg, mdigraph:new([bad_type]))
      ]
     }.
